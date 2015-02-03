ccbd7bcd24bc48d3967e22f3eb0a68a75f4541fb
Kristen Sundquist <kssundquist@gmail.com>
2015-02-02 20:31:20 -0800

    Initial commit with basic table view

---
+++ RottenTomatoesSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RottenTomatoesSwift
+//
+//  Created by Kristen on 2/2/15.
+//  Copyright (c) 2015 Kristen Sundquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+}
+
